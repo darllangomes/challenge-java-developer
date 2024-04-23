631acb050f550ec66ce948e47fa066ded072c56b
Darllan Gomes <darllangomes10@hotmail.com>
2024-04-23 10:02:59 -0300

    create client DTO

---
+++ src/main/java/br/com/neurotech/challenge/DTOS/NeuroTechClientDTO.java
@@ -0,0 +1,4 @@
+package br.com.neurotech.challenge.DTOS;
+
+public record NeuroTechClientDTO(Integer age, String name, Double income ) {
+}
--- src/main/java/br/com/neurotech/challenge/entity/NeurotechClient.java
+++ src/main/java/br/com/neurotech/challenge/entity/NeurotechClient.java
@@ -1,12 +1,32 @@
 package br.com.neurotech.challenge.entity;
 
+import br.com.neurotech.challenge.DTOS.NeuroTechClientDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 
 @Data
+@EqualsAndHashCode(of="id")
+@Entity(name="clients")
+@Table(name="clients")
+@AllArgsConstructor
+@NoArgsConstructor
 public class NeurotechClient {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String name;
 	private Integer age;
 	private Double income;
+
+	public NeurotechClient(NeuroTechClientDTO data){
+		this.age = data.age();
+		this.income = data.income();
+		this.name = data.name();
+	}
 
 }
